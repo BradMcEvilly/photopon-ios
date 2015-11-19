00e251696b832b5544d9573e4c93bb49a339804b
iBad <hayk@hayotsyan.com>
2015-11-19 21:54:06 +0300

    New coupon table cell

---
+++ Photopon/Photopon/CouponTableViewCell.h
@@ -0,0 +1,13 @@
+//
+//  CouponTableViewCell.h
+//  Photopon
+//
+//  Created by Hayk Hayotsyan on 19/11/15.
+//  Copyright © 2015 Photopon. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CouponTableViewCell : UITableViewCell
+
+@end
---
+++ Photopon/Photopon/CouponTableViewCell.m
@@ -0,0 +1,23 @@
+//
+//  CouponTableViewCell.m
+//  Photopon
+//
+//  Created by Hayk Hayotsyan on 19/11/15.
+//  Copyright © 2015 Photopon. All rights reserved.
+//
+
+#import "CouponTableViewCell.h"
+
+@implementation CouponTableViewCell
+
+- (void)awakeFromNib {
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
